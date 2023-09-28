fb3cc2ce391d771047e214954a9dfdafd55c3938
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-09-28 17:40:07 -0500

    Update challenges-1.sql

--- parte-1/sql-interview/challenges-1.sql
+++ parte-1/sql-interview/challenges-1.sql
@@ -29,8 +29,25 @@ select * from test.test_table_1;
 select * from test.test_table_2;
 
 --1. Como encuentro duplicados en una tabla. Dar un ejemplo mostrando duplicados de la columna orden en la tabla de ventas. (responder teoricamente)
-
+--Busco en la tabla solo las ordenes, uso un count para contarlas y agrupo y filtro las ordenes que aparecen mas de una vez
+select os.order_number
+, count(*)
+from stg.order_line_sale os
+group by os.order_number
+having count(*)>1
+ORDER BY count(*) desc
 --2. Como elimino duplicados? (responder teoricamente)
+-- Primero agrego una columna ID para tener una columna con valores únicos
+ALTER TABLE stg.order_line_sale
+ADD COLUMN id SERIAL PRIMARY KEY
+--Uso una consulta que me elimine los duplicados, fijandose que no se elimine la orden que inicialmente se encontró duplicada
+DELETE FROM stg.order_line_sale as ols
+WHERE EXISTS (
+	select 1
+    from stg.order_line_sale as sub_ols
+	where sub_ols.order_number=ols.order_number
+	AND sub_ols.id<>ols.id)
+
 
 --3. Cual es la diferencia entre UNION y UNION ALL. (responder teoricamente)
 
