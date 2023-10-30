83cfc790c7ed99a167713810bb6f5f350e3aaf2e
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-10-29 23:49:39 -0500

    Create stg_employee.sql

---
+++ parte-3/project/retail-data-warehouse/stg/stg_employee.sql
@@ -0,0 +1,18 @@
+-- Table: stg.employee
+
+DROP TABLE IF EXISTS stg.employee;
+
+CREATE TABLE IF NOT EXISTS stg.employee
+(
+    id integer NOT NULL DEFAULT nextval('employees_id_seq'::regclass),
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    surname character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    phone character varying(100) COLLATE pg_catalog."default",
+    country character varying(100) COLLATE pg_catalog."default",
+    province character varying(100) COLLATE pg_catalog."default",
+    store_id integer NOT NULL,
+    "position" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    CONSTRAINT employees_pkey PRIMARY KEY (id)
+);
