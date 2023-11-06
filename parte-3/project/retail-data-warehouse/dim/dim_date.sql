cde34266428c6a238e0eff9fbca3937f7e4ad884
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-06 16:28:58 -0500

    Create dim_date.sql

---
+++ parte-3/project/retail-data-warehouse/dim/dim_date.sql
@@ -0,0 +1,25 @@
+-- Table: dim.date
+
+DROP TABLE IF EXISTS dim.date;
+
+CREATE TABLE IF NOT EXISTS dim.date
+(
+	date date PRIMARY KEY,
+	month date, 
+	year date,
+	Dia_de_la_semana text,
+	is_weekend boolean,
+	month_label text,
+	fiscal_year date,
+	fiscal_year_label text,
+	fiscal_quarter_label text,
+	date_ly date,
+  constraint fk_date_date
+  foreign key (date)
+  references fct.inventory(date)
+  references fct.store_traffic(date)
+  references fct.fx_rate(month)
+  references fct.return_movements(date)
+  references fct.order_line_sale(date)
+
+);
