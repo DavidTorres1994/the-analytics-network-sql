bef48a791acb99000012f66c1cd873a79464a04f
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-06 14:19:37 -0500

    Update dim_product_master.sql

--- parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_product_master.sql
@@ -1,1 +1,23 @@
+-- Table: dim.product_master
+
+DROP TABLE IF EXISTS dim.product_master;
+
+CREATE TABLE IF NOT EXISTS dim.product_master
+(
+                              product_code    VARCHAR(10) primary key
+                            , name            VARCHAR(255)
+                            , category        VARCHAR(255)
+                            , subcategory     VARCHAR(255)
+                            , subsubcategory  VARCHAR(255)
+                            , material        VARCHAR(255)
+                            , color           VARCHAR(255)
+                            , origin          VARCHAR(255)
+                            , ean             bigint
+                            , is_active       boolean
+                            , has_bluetooth   boolean
+                            , size            VARCHAR(255),
+                      constraint fk_product_code_product_master
+                      foreign key (product_code)
+                      references dim.supplier(product_id)
+                 );
 
