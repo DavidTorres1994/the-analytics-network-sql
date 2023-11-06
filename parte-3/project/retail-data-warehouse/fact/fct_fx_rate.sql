51d225039c41256f63b83aadcae51e7723eec68a
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-06 17:15:49 -0500

    Create fct_fx_rate.sql

---
+++ parte-3/project/retail-data-warehouse/fact/fct_fx_rate.sql
@@ -0,0 +1,16 @@
+-- Table fct.fx_rate
+
+DROP TABLE IF EXISTS fct.fx_rate
+
+CREATE TABLE IF NOT EXISTS fct.fx_rate
+(
+                             month        DATE
+                            , fx_rate_usd_peso DECIMAL
+                            , fx_rate_usd_eur DECIMAL
+                            , fx_rate_usd_uru  DECIMAL
+            ,
+             constraint fk_month_fx_rate
+             foreign key (month)
+             references dim.date(month)
+  
+);
