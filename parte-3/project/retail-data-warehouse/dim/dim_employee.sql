936e304d86d2327b23380eb070ba98c99ae22fa2
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-06 14:56:14 -0500

    Update dim_employee.sql

--- parte-3/project/retail-data-warehouse/dim/dim_employee.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -1,1 +1,19 @@
+-- Table: dim.employee
+DROP TABLE IF EXISTS dim.employee;
 
+CREATE TABLE IF NOT EXISTS dim.employee
+(   id SERIAL PRIMARY KEY,
+    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    surname character varying(255) COLLATE pg_catalog."default" NOT NULL,
+    start_date date NOT NULL,
+    end_date date,
+    phone character varying(100) COLLATE pg_catalog."default",
+    country character varying(100) COLLATE pg_catalog."default",
+    province character varying(100) COLLATE pg_catalog."default",
+    store_id SMALLINT NOT NULL,
+    "position" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    constraint fk_store_id_employee
+    foreign key (store_id)
+    references dim.store_master(store_id)
+
+);
