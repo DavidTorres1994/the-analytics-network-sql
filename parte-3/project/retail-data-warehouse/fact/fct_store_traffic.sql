66660b282c0c8e730ec002f1c8f7892d99b65e6a
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-06 17:06:25 -0500

    Update fct_store_traffic.sql

--- parte-3/project/retail-data-warehouse/fact/fct_store_traffic.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_store_traffic.sql
@@ -1,1 +1,14 @@
-
+-- Table. fct.store_traffic
+DROP TABLE IF EXISTS fct.store_traffic;
+CREATE TABLE IF NOT EXISTS  fct.store_traffic
+                 (
+                              store_id SMALLINT
+                            , date  date
+                            , traffic SMALLINT
+                constraint fk_store_id_cost
+		            foreign key (store_id)
+		            references dim.store_master(store_id)
+                constraint fk_date_store_traffic
+                foreign key (date)
+                references dim.date(date)
+                 );
