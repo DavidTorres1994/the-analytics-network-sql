efb1b2adef855b319c0e4f765c6900febb24f547
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-07 21:02:16 -0500

    Update dim_employee.sql

--- parte-3/project/retail-data-warehouse/dim/dim_employee.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_employee.sql
@@ -3,15 +3,17 @@ DROP TABLE IF EXISTS dim.employee;
 
 CREATE TABLE IF NOT EXISTS dim.employee
 (   id SERIAL PRIMARY KEY,
-    name character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    surname character varying(255) COLLATE pg_catalog."default" NOT NULL,
-    start_date date NOT NULL,
-    end_date date,
-    phone character varying(100) COLLATE pg_catalog."default",
-    country character varying(100) COLLATE pg_catalog."default",
-    province character varying(100) COLLATE pg_catalog."default",
-    store_id SMALLINT NOT NULL,
-    "position" character varying(100) COLLATE pg_catalog."default" NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	surname VARCHAR(255) NOT NULL,
+	start_date DATE NOT NULL, 
+	end_date DATE,
+	phone VARCHAR(20),
+	country VARCHAR(100),
+	province VARCHAR(100),
+	store_id INT NOT NULL,
+	position VARCHAR(100) NOT NULL,
+    is_active boolean,
+    
     constraint fk_store_id_employee
     foreign key (store_id)
     references dim.store_master(store_id)
