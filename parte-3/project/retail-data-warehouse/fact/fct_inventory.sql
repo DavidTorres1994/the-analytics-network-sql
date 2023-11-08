f80711989bd678e5ec57abe5b020feec39d9751c
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-07 22:32:52 -0500

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_inventory.sql
@@ -1,22 +1,17 @@
 -- Table fct.inventory
-
-DROP TABLE IF EXISTS fct.inventory
-
+DROP TABLE IF EXISTS fct.inventory;
 CREATE TABLE IF NOT EXISTS fct.inventory
-(
-                             date   DATE
-                            , store_id  SMALLINT
-                            , item_id     VARCHAR(10)
-                            , initial SMALLINT
-                            , final   SMALLINT
-             constraint fk_store_id_inventory
-             foreign key (store_id)
-             references dim.store_master(store_id)
-             constraint fk_item_id_inventory
-             foreign key (item_id)
-             references dim.product_master(product_code)
-             constraint fk_date_inventory
-             foreign key (date)
-             references dim.date(date)
-  
-);
+   ( date DATE,
+     store_id SMALLINT,
+     item_id VARCHAR(10),
+     initial SMALLINT,
+     final SMALLINT,
+     constraint fk_store_id_inventory
+     foreign key (store_id)
+     references dim.store_master(store_id),
+     constraint fk_item_id_inventory
+     foreign key (item_id)
+     references dim.product_master(product_code),
+     constraint fk_date_inventory
+     foreign key (date)
+     references dim.date(date) );
