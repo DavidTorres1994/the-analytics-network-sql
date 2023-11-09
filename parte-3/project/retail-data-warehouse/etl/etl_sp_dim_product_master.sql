5cf280b545e475d787496d643bc39e138da54a79
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-08 22:34:30 -0500

    Update etl_sp_dim_product_master.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_product_master.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_product_master.sql
@@ -1,23 +1,28 @@
-create or replace procedure etl.sp.dim.product_master()
-language sql as $$
+CREATE OR REPLACE PROCEDURE etl.sp_dim_product_master()
+LANGUAGE plpgsql AS $$
 BEGIN
-  -- Añadir una columna si no existe
-  ALTER TABLE dim.prduct_master
-  ADD COLUMN IF NOT EXISTS brand VARCHAR(255); 
-  -- Actualizar la columna 'material'
-  UPDATE dim.prduct_master
-  SET material= 
-   CASE                                 
-     WHEN material IS NULL THEN 'Unknown'
-	   ELSE initcap(material)
-  END;
-	-- Actualizar la columna 'color'
-  UPDATE dim.product_master
-  SET color = 
+    -- Añadir una columna si no existe
+    ALTER TABLE dim.product_master
+    ADD COLUMN IF NOT EXISTS brand VARCHAR(255);
+    
+    -- Actualizar la columna 'material'
+    UPDATE dim.product_master
+    SET material = 
+        CASE 
+            WHEN material IS NULL THEN 'Unknown'
+            ELSE initcap(material)
+        END;
+    
+    -- Actualizar la columna 'color'
+    UPDATE dim.product_master
+    SET color = 
         CASE 
             WHEN color IS NULL THEN 'Unknown'
             ELSE initcap(color)
         END;
-END;                        
-$$
-;
+END;
+$$;
+
+
+
+
