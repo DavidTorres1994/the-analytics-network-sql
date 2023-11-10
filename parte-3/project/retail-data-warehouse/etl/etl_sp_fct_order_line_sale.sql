4edeb3fc16da204c4aa34ae14d8eaafb9ff7a00e
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-10 00:27:22 -0500

    Update etl_sp_fct_order_line_sale.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
@@ -4,6 +4,8 @@ LANGUAGE sql AS $$
     INSERT INTO fct.order_line_sale(order_number,product,store,date,quantity,sale,promotion,tax,credit,currency,pos,is_walkout)
     SELECT order_number,product,store,date,quantity,sale,promotion,tax,credit,currency,pos,is_walkout
     FROM stg.order_line_sale;
+
+    ALTER TABLE fct.order_line_sale
+        ADD COLUMN IF NOT EXISTS line_key SERIAL UNIQUE;
   $$;
-  
 call etl.sp_fct_order_line_sale()
