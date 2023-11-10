b25a7d761d08e3d446f1750fb65be211f6d80667
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-09 22:53:03 -0500

    Update etl_sp_fct_store_traffic.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_store_traffic.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_store_traffic.sql
@@ -1,1 +1,16 @@
+CREATE OR REPLACE PROCEDURE etl.sp_fct_store_traffic()
+LANGUAGE sql AS $$    
+   with store_traffic as (
+    SELECT sc.store_id, TO_DATE(sc.date,'YYYY-MM-DD')AS date, sc.traffic
+    FROM stg.super_store_count sc
+    UNION ALL
+    SELECT mc.store_id, TO_DATE(CAST(mc.date AS VARCHAR),'YYYYMMDD')AS date, mc.traffic
+    FROM stg.market_count mc)
+  
+    INSERT INTO fct.store_traffic(store_id,date,traffic)
+    SELECT store_id,date,traffic
+    FROM store_traffic;
+    
 
+$$;
+call etl.sp_fct_store_traffic() 
