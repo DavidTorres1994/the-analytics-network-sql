89701701303460aee0629807b5ab3de3357fa4de
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-10 00:55:03 -0500

    Update etl_sp_fct_fx_rate.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
@@ -3,7 +3,11 @@ LANGUAGE sql AS $$
   
     INSERT INTO fct.fx_rate(month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru)
     SELECT month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru
-    FROM stg.monthly_average_fx_rate;
+     FROM stg.monthly_average_fx_rate mar
+    WHERE NOT EXISTS (
+        SELECT 1
+        FROM fct.fx_rate fr
+        WHERE fr.month = mar.month);
   $$;
   
 call etl.sp_fct_fx_rate()
