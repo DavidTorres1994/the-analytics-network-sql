024c931ab18b08a54dce46e9938d672f49a3f0aa
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-09 20:28:05 -0500

    Update etl_sp_dim_cost.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
@@ -1,15 +1,13 @@
 CREATE OR REPLACE PROCEDURE etl.sp_dim_cost()
-LANGUAGE plpgsql AS $$
-BEGIN
-    INSERT INTO dim.cost(product_id, cost_usd)
-    SELECT dp.product_code, dc.cost_usd
+LANGUAGE sql AS $$
+    with STG_cost as (SELECT dp.product_code, dc.product_cost_usd
     FROM dim.product_master dp
-    LEFT JOIN dim.cost dc ON dp.product_code = dc.product_id;
+    INNER JOIN stg.cost dc ON dp.product_code = dc.product_code)
 
-    EXCEPTION
-        WHEN unique_violation THEN
-            UPDATE dim.cost
-            SET cost_usd = EXCLUDED.cost_usd
-            WHERE product_id = EXCLUDED.product_id;
-END;
+    INSERT INTO dim.cost(product_id, cost_usd)
+    SELECT product_code,product_cost_usd
+    FROM stg_cost
+    ON CONFLICT(product_id) DO UPDATE
+    SET cost_usd = EXCLUDED.cost_usd;
 $$;
+call etl.sp_dim_cost()
