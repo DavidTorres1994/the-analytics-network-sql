059f3d8f5319cd2e53458fdc39c64cb4fd7e3a10
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-12 00:20:21 -0500

    Create etl_sp_backup_dim_fct.sql

---
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_backup_dim_fct.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE PROCEDURE etl.sp_backup_dim_fct()
+LANGUAGE sql AS $$    
+--backup tabla fct_order_line_sale    
+CREATE TABLE IF NOT EXISTS bkp.bkp_fct_order_line_sale_20231111 AS
+SELECT *
+FROM fct.order_line_sale;
+--backup tabla fct_fx_rate    
+CREATE TABLE IF NOT EXISTS bkp.bkp_fct_fx_rate_20231111 AS
+SELECT *
+FROM fct.fx_rate;   
+--backup tabla fct_inventory    
+CREATE TABLE IF NOT EXISTS bkp.bkp_fct_inventory_20231111 AS
+SELECT *
+FROM fct.inventory; 
+--backup tabla fct_return_movements    
+CREATE TABLE IF NOT EXISTS bkp.bkp_fct_return_movements_20231111 AS
+SELECT *
+FROM fct.return_movements; 
+--backup tabla fct_store_traffic    
+CREATE TABLE IF NOT EXISTS bkp.bkp_fct_store_traffic_20231111 AS
+SELECT *
+FROM fct.store_traffic;   
+--backup tabla dim_cost    
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_cost_20231111 AS
+SELECT *
+FROM dim.cost; 
+--backup tabla dim_date    
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_date_20231111 AS
+SELECT *
+FROM dim.date; 
+--backup tabla dim_employee    
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_employee_20231111 AS
+SELECT *
+FROM dim.employee; 
+--backup tabla dim_product_master   
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_product_master_20231111 AS
+SELECT *
+FROM dim.product_master;
+--backup tabla dim_store_master   
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_store_master_20231111 AS
+SELECT *
+FROM dim.store_master; 
+--backup tabla dim_supplier   
+CREATE TABLE IF NOT EXISTS bkp.bkp_dim_supplier_20231111 AS
+SELECT *
+FROM dim.supplier;
+  
+  $$;
+
+call etl.sp_backup_dim_fct()
