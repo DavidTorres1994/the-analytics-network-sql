123ce6b105cf7edd3635ceaf9e696f1e1f5df20d
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-12 00:04:30 -0500

    Update dim_date.sql

--- parte-3/project/retail-data-warehouse/dim/dim_date.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_date.sql
@@ -13,14 +13,4 @@ CREATE TABLE IF NOT EXISTS dim.date
 	fiscal_year date,
 	fiscal_year_label text,
 	fiscal_quarter_label text,
-	date_ly date) /*,
-  constraint fk_date_date
-  foreign key (date)
-  references fct.inventory(date)
-  references fct.store_traffic(date)
-  references fct.return_movements(date)
-  references fct.order_line_sale(date),
-  constraint fk_month_date
-  foreign key (month)	
-  references fct.fx_rate(month)
-);*/
+	date_ly date) 
