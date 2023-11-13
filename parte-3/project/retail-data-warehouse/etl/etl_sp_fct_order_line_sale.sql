48f210ac92d8fb88b117df33ffd51394d9fc9acc
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:40:17 -0500

    Update etl_sp_fct_order_line_sale.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_fct_order_line_sale()
-LANGUAGE sql AS $$    
+LANGUAGE plpgsql AS $$    
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;  
      ALTER TABLE fct.order_line_sale
         ADD COLUMN IF NOT EXISTS line_key SERIAL UNIQUE;
     INSERT INTO fct.order_line_sale(order_number,product,store,date,quantity,sale,promotion,tax,credit,currency,pos,is_walkout)
@@ -12,6 +14,9 @@ LANGUAGE sql AS $$
 		 and  f.product = ol.product
 		 and  f.store = ol.store
 		 and  f.date = ol.date
-    );  
+    );
+--sp de logg
+    call etl.log('fct.order_line_sale', current_date, 'etl.sp_fct_order_line_sale',username);
+END;
   $$;
 call etl.sp_fct_order_line_sale()
