9dc478eabf65dcf45d4afaeb07b61e9bad212654
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-12 23:25:32 -0500

    Update etl_sp_fct_store_traffic.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_store_traffic.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_store_traffic.sql
@@ -1,21 +1,29 @@
 CREATE OR REPLACE PROCEDURE etl.sp_fct_store_traffic()
-LANGUAGE sql AS $$    
+LANGUAGE plpgsql as $$
+-- declaracion de variables
+DECLARE username varchar(10) := current_user;
+BEGIN username := current_user;
+--transformación
    with store_traffic as (
     SELECT sc.store_id, TO_DATE(sc.date,'YYYY-MM-DD')AS date, sc.traffic
     FROM stg.super_store_count sc
     UNION ALL
     SELECT mc.store_id, TO_DATE(CAST(mc.date AS VARCHAR),'YYYYMMDD')AS date, mc.traffic
     FROM stg.market_count mc)
-  
+--insert  
     INSERT INTO fct.store_traffic(store_id,date,traffic)
     SELECT store_id,date,traffic
     FROM store_traffic st
+    --ON conflict(store_id, date) do nothing;
 	WHERE NOT EXISTS (
         SELECT 1
         FROM fct.store_traffic ft
         WHERE ft.store_id = st.store_id
 		and ft.date=st.date
-	              );
+	              ); 
+    --sp de logg
+    call etl.log('fct.store_traffic', current_date, 'sp_fct_store_traffic',username);
+  END;
   $$;
   
 call etl.sp_fct_store_traffic()
