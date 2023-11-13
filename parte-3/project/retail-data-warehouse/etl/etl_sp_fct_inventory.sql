fe96bb747b725838aa7044594ac907fccedfe1a1
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:36:04 -0500

    Update etl_sp_fct_inventory.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_fct_inventory()
-LANGUAGE sql AS $$    
+LANGUAGE plpgsql AS $$    
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;  
      
     INSERT INTO fct.inventory(date,store_id,item_id,initial,final)
     SELECT i.date,i.store_id,i.item_id,i.initial,i.final
@@ -11,6 +13,9 @@ LANGUAGE sql AS $$
 		 and  f.store_id = i.store_id
 		 and  f.item_id = i.item_id
 
-    );  
+    );
+--sp de logg
+    call etl.log('fct.inventory', current_date, 'etl.sp_fct_inventory',username);
+END;
   $$;
 call etl.sp_fct_inventory()
