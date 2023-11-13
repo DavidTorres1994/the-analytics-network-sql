281d8f72f9c43bd4e46f9bcc22768e6f23cca37a
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:24:49 -0500

    Update etl_sp_dim_supplier_product.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_supplier_product.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_supplier_product.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_dim_supplier_product()
-LANGUAGE sql AS $$
+LANGUAGE plpgsql AS $$   
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;
     with stg_supplier as (SELECT dp.product_code, s.name, s.is_primary
     FROM dim.product_master dp
     INNER JOIN stg.supplier s ON dp.product_code = s.product_id
@@ -8,6 +10,9 @@ LANGUAGE sql AS $$
     INSERT INTO dim.supplier(product_id, name,is_primary)
     SELECT product_code,name,is_primary
     FROM stg_supplier;
+--sp de logg
+    call etl.log('dim.supplier', current_date, 'etl.sp_dim_supplier_product',username);
+END;
 $$;
 
 call etl.sp_dim_supplier_product()
