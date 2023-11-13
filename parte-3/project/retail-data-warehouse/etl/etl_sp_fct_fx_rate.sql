0fe69a2a6b3743cc13e15cb4ac35a1188e09916b
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:32:11 -0500

    Update etl_sp_fct_fx_rate.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_fct_fx_rate()
-LANGUAGE sql AS $$    
+LANGUAGE plpgsql AS $$    
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;  
   
     INSERT INTO fct.fx_rate(month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru)
     SELECT month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru
@@ -8,6 +10,9 @@ LANGUAGE sql AS $$
         SELECT 1
         FROM fct.fx_rate fr
         WHERE fr.month = mar.month);
+ --sp de logg
+    call etl.log('fct.fx_rate', current_date, 'etl.sp_fct_fx_rate',username);
+END;
   $$;
   
 call etl.sp_fct_fx_rate()
