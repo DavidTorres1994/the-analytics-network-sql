25fb725f8b14c9092d7f859e2d05a1dc55ac22ce
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:03:56 -0500

    Update etl_sp_dim_employee.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_employee.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_employee.sql
@@ -1,16 +1,19 @@
 CREATE OR REPLACE PROCEDURE etl.sp_dim_employee()
 LANGUAGE plpgsql AS $$    
-BEGIN
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;
     INSERT INTO dim.employee(id,employee_key,name,surname,start_date,end_date,phone,country,province,store_id,position,current_flag,effective_start_date,effective_end_date)
     SELECT id,employee_key,name,surname,start_date,end_date,phone,country,province,store_id,position,current_flag,effective_start_date,effective_end_date
-    FROM stg.employee;
+    FROM stg.employee
+    on conflict(id) do nothing;
     -- Añadir una columna si no existe
      ALTER TABLE dim.employee
         ADD COLUMN IF NOT EXISTS is_active BOOLEAN;
 
         ALTER TABLE dim.employee
         ADD COLUMN IF NOT EXISTS duration INT;
-    
+--sp de logg
+    call etl.log('dim.employee', current_date, 'etl.sp_dim_employee',username);    
     
 END;
 $$;
