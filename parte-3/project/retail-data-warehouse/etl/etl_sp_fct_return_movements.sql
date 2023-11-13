e35e3baed196ec1172bf9cad0dc4bd6fe830a3ac
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-13 00:43:50 -0500

    Update etl_sp_fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_fct_return_movements()
-LANGUAGE sql AS $$    
+LANGUAGE plpgsql AS $$    
+DECLARE username varchar(10) := current_user;	
+BEGIN username := current_user;    
      
     INSERT INTO fct.return_movements(order_id,return_id,item,quantity,movement_id,from_location,to_location,received_by,date)
     SELECT r.order_id,r.return_id,r.item,r.quantity,r.movement_id,r.from_location,r.to_location,r.received_by,r.date
@@ -11,6 +13,9 @@ LANGUAGE sql AS $$
 		 and  f.return_id = r.return_id
 		 and  f.item = r.item
      and  f.movement_id = r.movement_id
-    );  
+    ); 
+--sp de logg
+    call etl.log('fct.return_movements', current_date, 'etl.sp_fct_return_movements',username);
+END;
   $$;
 call etl.sp_fct_return_movements()
