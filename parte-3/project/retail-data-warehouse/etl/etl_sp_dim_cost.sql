eb359dc22110b91a3eb3421b549e38f96c878e04
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-12 23:35:10 -0500

    Update etl_sp_dim_cost.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_cost.sql
@@ -1,5 +1,7 @@
 CREATE OR REPLACE PROCEDURE etl.sp_dim_cost()
-LANGUAGE sql AS $$
+LANGUAGE plpgsql as $$
+DECLARE username varchar(10) := current_user;
+BEGIN username := current_user;
     with stg_cost as (SELECT dp.product_code, dc.product_cost_usd
     FROM dim.product_master dp
     INNER JOIN stg.cost dc ON dp.product_code = dc.product_code)
@@ -9,5 +11,8 @@ LANGUAGE sql AS $$
     FROM stg_cost
     ON CONFLICT(product_id) DO UPDATE
     SET cost_usd = EXCLUDED.cost_usd;
+ --sp de logg
+    call etl.log('dim.cost', current_date, 'etl.sp_dim_cost',username);
+  END;
 $$;
 call etl.sp_dim_cost()
