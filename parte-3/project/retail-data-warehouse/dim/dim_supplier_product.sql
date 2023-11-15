4dc60722a57fc2d4e12daeb5b37dcbf2f877fdb3
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-15 01:22:13 -0500

    Update dim_supplier_product.sql

--- parte-3/project/retail-data-warehouse/dim/dim_supplier_product.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_supplier_product.sql
@@ -2,7 +2,7 @@
 
 DROP TABLE IF EXISTS dim.supplier;
 CREATE TABLE IF NOT EXISTS dim.supplier
-(  
+(   
     product_id VARCHAR(10) COLLATE pg_catalog."default",
     name character varying(255) COLLATE pg_catalog."default",
     is_primary boolean,
