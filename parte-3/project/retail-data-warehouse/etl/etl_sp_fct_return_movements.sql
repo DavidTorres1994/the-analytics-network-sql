b9b1b65d55d2d1b2e11aa047c10a64cbc0d1ae64
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 22:33:45 -0500

    Update etl_sp_fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_return_movements.sql
@@ -6,14 +6,15 @@ BEGIN username := current_user;
     INSERT INTO fct.return_movements(order_id,return_id,item,quantity,movement_id,from_location,to_location,received_by,date)
     SELECT r.order_id,r.return_id,r.item,r.quantity,r.movement_id,r.from_location,r.to_location,r.received_by,r.date
     FROM stg.return_movements r
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM fct.return_movements f
-        WHERE f.order_id = r.order_id
-		 and  f.return_id = r.return_id
-		 and  f.item = r.item
-     and  f.movement_id = r.movement_id
-    ); 
+    on conflict(order_id, movement_id) do nothing; 
+    --WHERE NOT EXISTS (
+      --  SELECT 1
+       -- FROM fct.return_movements f
+       -- WHERE f.order_id = r.order_id
+--		 and  f.return_id = r.return_id
+--		 and  f.item = r.item
+  --   and  f.movement_id = r.movement_id
+    --); 
 --sp de logg
     call etl.log('fct.return_movements', current_date, 'etl.sp_fct_return_movements',username);
 END;
