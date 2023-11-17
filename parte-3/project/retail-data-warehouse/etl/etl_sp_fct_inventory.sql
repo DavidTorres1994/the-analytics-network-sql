98fb5c53b0e6f25c2b9679a55adba1859a107a5b
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 22:05:33 -0500

    Update etl_sp_fct_inventory.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_inventory.sql
@@ -6,14 +6,15 @@ BEGIN username := current_user;
     INSERT INTO fct.inventory(date,store_id,item_id,initial,final)
     SELECT i.date,i.store_id,i.item_id,i.initial,i.final
     FROM stg.inventory i
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM fct.inventory f
-        WHERE f.date = i.date
-		 and  f.store_id = i.store_id
-		 and  f.item_id = i.item_id
+     on conflict(date, store_id, item_id) do nothing; 
+    --WHERE NOT EXISTS (
+    --    SELECT 1
+    --    FROM fct.inventory f
+    --    WHERE f.date = i.date
+	--	 and  f.store_id = i.store_id
+	--	 and  f.item_id = i.item_id
 
-    );
+    --);
 --sp de logg
     call etl.log('fct.inventory', current_date, 'etl.sp_fct_inventory',username);
 END;
