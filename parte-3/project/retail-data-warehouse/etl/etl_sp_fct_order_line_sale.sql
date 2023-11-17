238a84420ae8b93bc987d9126f2fd5d0683f19f2
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 22:18:42 -0500

    Update etl_sp_fct_order_line_sale.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_order_line_sale.sql
@@ -7,14 +7,15 @@ BEGIN username := current_user;
     INSERT INTO fct.order_line_sale(order_number,product,store,date,quantity,sale,promotion,tax,credit,currency,pos,is_walkout)
     SELECT ol.order_number,ol.product,ol.store,ol.date,ol.quantity,ol.sale,ol.promotion,ol.tax,ol.credit,ol.currency,ol.pos,ol.is_walkout
     FROM stg.order_line_sale ol
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM fct.order_line_sale f
-        WHERE f.order_number = ol.order_number
-		 and  f.product = ol.product
-		 and  f.store = ol.store
-		 and  f.date = ol.date
-    );
+    on conflict(order_number, product) do nothing; 
+    --WHERE NOT EXISTS (
+    --    SELECT 1
+     --   FROM fct.order_line_sale f
+     --   WHERE f.order_number = ol.order_number
+	--	 and  f.product = ol.product
+	--	 and  f.store = ol.store
+	--	 and  f.date = ol.date
+    --);
 --sp de logg
     call etl.log('fct.order_line_sale', current_date, 'etl.sp_fct_order_line_sale',username);
 END;
