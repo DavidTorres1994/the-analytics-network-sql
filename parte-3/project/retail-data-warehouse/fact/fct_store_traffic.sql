69e71286b74a9aac51ebad021973d8d5ceffbf33
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 22:42:52 -0500

    Update fct_store_traffic.sql

--- parte-3/project/retail-data-warehouse/fact/fct_store_traffic.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_store_traffic.sql
@@ -5,7 +5,9 @@ CREATE TABLE IF NOT EXISTS  fct.store_traffic
                               store_id SMALLINT
                             , date  date
                             , traffic SMALLINT
-                ,constraint fk_store_id_cost
+	        , constraint pk_store_traffic
+	          primary key (store_id,date)
+                ,constraint fk_store_id_store_traffic
 		            foreign key (store_id)
 		            references dim.store_master(store_id)
                 ,constraint fk_date_store_traffic
