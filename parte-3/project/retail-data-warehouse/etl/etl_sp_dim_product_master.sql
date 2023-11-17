f11e9248f54a5e44f14262e921434b0bd4a74b1d
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 21:22:18 -0500

    Update etl_sp_dim_product_master.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_dim_product_master.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_dim_product_master.sql
@@ -5,11 +5,22 @@ BEGIN username := current_user;
     -- Añadir una columna si no existe
     ALTER TABLE dim.product_master
     ADD COLUMN IF NOT EXISTS brand VARCHAR(255);
-   
-    
+   -- TRUNCATE TABLE dim.product_master;
+     with cte as (
+  select product_code,name,category,subcategory,subsubcategory,material,color,origin,ean,is_active,has_bluetooth,size,
+	    CASE 
+        WHEN lower(name) LIKE '%samsung%' THEN 'Samsung'
+        WHEN lower(name) LIKE '%philips%' THEN 'Phillips'
+        WHEN lower(name) LIKE 'levi%' THEN 'Levis'
+        WHEN lower(name) LIKE 'jbl%' THEN 'JBL'
+        WHEN lower(name) LIKE '%motorola%' THEN 'Motorola'
+        WHEN lower(name) LIKE 'tommy%' THEN 'TH'
+        ELSE 'Unknown' end as brand
+	  from stg.product_master
+  )
     INSERT INTO dim.product_master(product_code,name,category,subcategory,subsubcategory,material,color,origin,ean,is_active,has_bluetooth,size,brand)
     SELECT *
-    FROM stg.product_master 
+    FROM cte 
     ON CONFLICT(product_code) DO UPDATE
     SET product_code = excluded.product_code,
         name = EXCLUDED.name,
