faed3c51d8216ba0215f5147032bfa9c74994223
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 22:28:20 -0500

    Update fct_return_movements.sql

--- parte-3/project/retail-data-warehouse/fact/fct_return_movements.sql
+++ parte-3/project/retail-data-warehouse/fact/fct_return_movements.sql
@@ -11,6 +11,8 @@ CREATE TABLE IF NOT EXISTS fct.return_movements
     to_location varchar(255),
     received_by varchar(255),
     date date,
+  constraint pk_return_movements
+  primary key(order_id,movement_id),
   constraint fk_item_return_movements
   foreign key (item)
   references dim.product_master(product_code),
