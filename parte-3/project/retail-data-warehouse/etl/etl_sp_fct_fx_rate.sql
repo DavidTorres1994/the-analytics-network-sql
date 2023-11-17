903701af2785f2d85e79bf2608d40c117fcda05f
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2023-11-16 21:44:33 -0500

    Update etl_sp_fct_fx_rate.sql

--- parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
+++ parte-3/project/retail-data-warehouse/etl/etl_sp_fct_fx_rate.sql
@@ -6,10 +6,11 @@ BEGIN username := current_user;
     INSERT INTO fct.fx_rate(month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru)
     SELECT month,fx_rate_usd_peso,fx_rate_usd_eur,fx_rate_usd_uru
      FROM stg.monthly_average_fx_rate mar
-    WHERE NOT EXISTS (
-        SELECT 1
-        FROM fct.fx_rate fr
-        WHERE fr.month = mar.month);
+     on conflict(month) do nothing; 
+   -- WHERE NOT EXISTS (
+   --     SELECT 1
+    --    FROM fct.fx_rate fr
+    --    WHERE fr.month = mar.month);
  --sp de logg
     call etl.log('fct.fx_rate', current_date, 'etl.sp_fct_fx_rate',username);
 END;
