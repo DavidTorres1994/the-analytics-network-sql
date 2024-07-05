fced419b5201fd2553a3b0946ba8920121e3ac84
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-05 00:15:03 -0500

    Update dim_supplier_product.sql

--- parte-3/project/retail-data-warehouse2/dim/dim_supplier_product.sql
+++ parte-3/project/retail-data-warehouse2/dim/dim_supplier_product.sql
@@ -1,1 +1,13 @@
+-- Table: dim.supplier
 
+DROP TABLE IF EXISTS dim.supplier;
+
+CREATE TABLE IF NOT EXISTS dim.supplier
+(
+    product_id character varying(255) COLLATE pg_catalog."default",
+    name character varying(255) COLLATE pg_catalog."default",
+    is_primary boolean
+    constraint fk_product_id_supplier
+		foreign key (product_id)
+		references dim.product_master(product_code)
+);
