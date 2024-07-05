b81eba6a7e88505e23b87578ef271c0f191d5aee
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-05 00:04:35 -0500

    Update dim_store_master.sql

--- parte-3/project/retail-data-warehouse2/dim/dim_store_master.sql
+++ parte-3/project/retail-data-warehouse2/dim/dim_store_master.sql
@@ -1,1 +1,16 @@
-
+/* Crea tabla store_master
+Tabla maestra de tiendas 
+*/
+DROP TABLE IF EXISTS dim.store_master;
+      
+CREATE TABLE dim.store_master
+                 (
+                              store_id  SMALLINT
+                            , country           VARCHAR(100)
+                            , province      VARCHAR(100)
+                            , city         VARCHAR(100)
+                            , address      VARCHAR(255)
+                            , name         VARCHAR(255)
+                            , type           VARCHAR(100)
+                            , start_date DATE
+                 );
