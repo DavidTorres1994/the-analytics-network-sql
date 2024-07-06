aa0ca611c254f02471d07adc27e0218a62b7618e
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-05 22:59:57 -0500

    Update fct_inventory.sql

--- parte-3/project/retail-data-warehouse2/fct/fct_inventory.sql
+++ parte-3/project/retail-data-warehouse2/fct/fct_inventory.sql
@@ -5,17 +5,19 @@ Conteo de inventario al inicio y final del dia por fecha, tienda y codigo
 */
 CREATE TABLE fct.inventory
                  (
-					 		                date   DATE
+			    date   DATE
                             , store_id  SMALLINT
                             , item_id     VARCHAR(10)
                             , initial SMALLINT
                             , final   SMALLINT
+	                   ,  constraint pk_inventory
+	                      primary key (date,store_id,item_id)
                             , constraint fk_date_inventory
                               foreign key (date)
-                              references dim.date(date)
+                              references dim.date(date),
                               constraint fk_store_id_inventory
                               foreign key (store_id)
-                              references dim.store_master(store_id)
+                              references dim.store_master(store_id),
                               constraint fk_item_id_inventory
                               foreign key (item_id)
                               references dim.product_master(product_code)
