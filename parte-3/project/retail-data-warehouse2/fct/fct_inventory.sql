de4f86658040d986890f7e926f4b72a50252618a
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-05 22:07:59 -0500

    Create fct_inventory.sql

---
+++ parte-3/project/retail-data-warehouse2/fct/fct_inventory.sql
@@ -0,0 +1,23 @@
+DROP TABLE IF EXISTS fct.inventory;
+    
+/* Crea tabla inventory
+Conteo de inventario al inicio y final del dia por fecha, tienda y codigo
+*/
+CREATE TABLE fct.inventory
+                 (
+					 		                date   DATE
+                            , store_id  SMALLINT
+                            , item_id     VARCHAR(10)
+                            , initial SMALLINT
+                            , final   SMALLINT
+                            , constraint fk_date_inventory
+                              foreign key (date)
+                              references dim.date(date)
+                              constraint fk_store_id_inventory
+                              foreign key (store_id)
+                              references dim.store_master(store_id)
+                              constraint fk_item_id_inventory
+                              foreign key (item_id)
+                              references dim.product_master(product_code)
+                              
+                 );
