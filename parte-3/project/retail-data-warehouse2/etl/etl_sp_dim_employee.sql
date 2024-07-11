f456e75aaf5bec2bdb2fc4a709b743b541f3eea8
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-10 23:16:46 -0500

    Create etl_sp_dim_employee.sql

---
+++ parte-3/project/retail-data-warehouse2/etl/etl_sp_dim_employee.sql
@@ -0,0 +1,26 @@
+create or replace procedure etl.sp_dim_employee()
+language plpgsql as $$ 
+--declarar variable
+declare user_name varchar(10) := current_user;
+begin user_name := current_user;
+ alter table dim.employee
+ add column if not exists is_active boolean;
+ alter table dim.employee
+ add column if not exists duration smallint;
+ with cte as (
+   select id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
+	 position, current_flag,effective_start_date,effective_end_date
+	from stg.employee)
+ insert into dim.employee(id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
+	 position, current_flag,effective_start_date,effective_end_date)
+ select id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
+	 position, current_flag,effective_start_date,effective_end_date
+ from cte
+ on conflict(id) do nothing;
+ 
+	--sp de logg
+    call etl.log('dim.employee', current_date,'etl.sp_dim_employee',user_name);
+    END;
+$$;
+
+call etl.sp_dim_employee()
