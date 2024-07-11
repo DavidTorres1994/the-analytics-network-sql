95537c516988ab726a1d6a2c9f56f948b37cf6c1
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-10 23:38:02 -0500

    Update etl_sp_dim_employee.sql

--- parte-3/project/retail-data-warehouse2/etl/etl_sp_dim_employee.sql
+++ parte-3/project/retail-data-warehouse2/etl/etl_sp_dim_employee.sql
@@ -7,20 +7,15 @@ begin user_name := current_user;
  add column if not exists is_active boolean;
  alter table dim.employee
  add column if not exists duration smallint;
- with cte as (
-   select id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
-	 position, current_flag,effective_start_date,effective_end_date
-	from stg.employee)
+
  insert into dim.employee(id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
 	 position, current_flag,effective_start_date,effective_end_date)
  select id,employee_key,name, surname,start_date,end_date,phone,country,province,store_id,
 	 position, current_flag,effective_start_date,effective_end_date
- from cte
+ from stg.employee
  on conflict(id) do nothing;
  
 	--sp de logg
     call etl.log('dim.employee', current_date,'etl.sp_dim_employee',user_name);
     END;
 $$;
-
-call etl.sp_dim_employee()
