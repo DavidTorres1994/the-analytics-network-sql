1d34a2d0622f69cde279b02397b4fe87f2724524
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-11 21:19:47 -0500

    Update etl_sp_fct_store_traffic.sql

--- parte-3/project/retail-data-warehouse2/etl/etl_sp_fct_store_traffic.sql
+++ parte-3/project/retail-data-warehouse2/etl/etl_sp_fct_store_traffic.sql
@@ -4,10 +4,16 @@ language plpgsql as $$
 declare user_name varchar(10) := current_user;
 begin user_name := current_user;
 
+with cte as (
+select mc.store_id, TO_DATE(CAST(mc.date AS VARCHAR),'YYYYMMDD') AS date, mc.traffic 
+from stg.market_count mc
+UNION ALL
+select sc.store_id, TO_DATE(sc.date,'YYYY-MM-DD') AS date, sc.traffic 
+from stg.super_store_count sc)
  
  insert into fct.store_traffic(store_id,date,traffic)
  select store_id,date,traffic
- from stg.vw_store_traffic
+ from cte
  on conflict(store_id,date) do nothing;
  
 	--sp de logg
