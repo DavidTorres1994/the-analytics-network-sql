dc81eba71692f97d4f2ef299229849bce5df658c
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-15 23:37:15 -0500

    Update etl_sp_dim_supplier_product.sql

--- parte-3/project/retail-data-warehouse2/etl/etl_sp_dim_supplier_product.sql
+++ parte-3/project/retail-data-warehouse2/etl/etl_sp_dim_supplier_product.sql
@@ -3,8 +3,19 @@ language plpgsql as $$
 --declarar variable
 declare user_name varchar(10) := current_user;
 begin user_name := current_user;
+
+    IF NOT EXISTS (
+        SELECT 1
+        FROM pg_constraint
+        WHERE conname = 'unique_product_id'
+    ) THEN
+        ALTER TABLE dim.supplier
+        ADD CONSTRAINT unique_product_id UNIQUE (product_id);
+    END IF;
+
  alter table dim.supplier
  add column if not exists supplier_key  serial unique; 
+
  with cte as (
    select s.product_id,s.name,s.is_primary
 	from stg.product_master pm
@@ -13,7 +24,7 @@ begin user_name := current_user;
  insert into dim.supplier(product_id,name,is_primary)
  select product_id,name,is_primary
  from cte
- on conflict(supplier_key) do nothing;
+ on conflict(product_id) do nothing;
  
 	--sp de logg
     call etl.log('dim.supplier', current_date,'etl.sp_dim_supplier_product',user_name);
@@ -21,3 +32,4 @@ begin user_name := current_user;
 $$;
 
 call etl.sp_dim_supplier_product()
+
