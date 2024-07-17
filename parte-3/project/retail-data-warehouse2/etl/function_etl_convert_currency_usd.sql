e6ee160dfec693accb2648e346d8730d36ea45c0
DavidTorres1994 <122503793+DavidTorres1994@users.noreply.github.com>
2024-07-16 23:53:53 -0500

    Create function_etl_convert_currency_usd.sql

---
+++ parte-3/project/retail-data-warehouse2/etl/function_etl_convert_currency_usd.sql
@@ -0,0 +1,13 @@
+create or replace function etl.convert_currency_usd(valor decimal(18,5),fecha date, moneda varchar(3)) returns decimal(18,5) as $$
+select
+coalesce(round(valor/(case
+			  when moneda ='EUR' then mfx.fx_rate_usd_eur
+			  when moneda ='ARS' then mfx.fx_rate_usd_peso
+			  when moneda ='URU' then mfx.fx_rate_usd_uru
+			  else 0 end),2),0) as valor_usd
+from fct.fx_rate mfx
+where date_trunc( 'month', mfx.month) = date_trunc('month', fecha)
+
+;
+$$ language sql;
+
